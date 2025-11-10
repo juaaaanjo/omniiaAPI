524e3e6b9dd40b8dc2f0bb566dac4a5f686bb607
Juan Jose Perez <juan.perez@parqco.com>
2025-11-09 21:15:47 -0500

    chore(env): remove shopify api credentials from example fix(database): ensure mongodb uri is defined

--- src/config/database.js
+++ src/config/database.js
@@ -1,12 +1,19 @@
 const mongoose = require('mongoose');
 const logger = require('../utils/logger');
+const config = require('./env');
 
 /**
  * Connect to MongoDB database
  */
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+    const uri = config.mongodbUri;
+
+    if (!uri) {
+      throw new Error('MONGODB_URI is not defined. Set the environment variable or update config.env.');
+    }
+
+    const conn = await mongoose.connect(uri, {
       maxPoolSize: 10,
       serverSelectionTimeoutMS: 5000,
       socketTimeoutMS: 45000,
